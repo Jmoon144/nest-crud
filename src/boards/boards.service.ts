d7a9e1f76f1ebc6c2719436993b71b775de37536
박준영 <sjwm98@gmail.com>
2021-09-01 00:23:17 +0900

    Add: 게시물 기능 수정

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -39,8 +39,8 @@ export class BoardsService {
     }
 
     // 리턴값 없기 때문에 void
-    async deleteBoard(id: number): Promise<void> {
-        const result = await this.boardRepository.delete(id);
+    async deleteBoard(id: number, user: User): Promise<void> {
+        const result = await this.boardRepository.delete({id, user});
 
         if(result.affected === 0) {
             throw new NotFoundException(`Can't find Board with id ${id}`);
