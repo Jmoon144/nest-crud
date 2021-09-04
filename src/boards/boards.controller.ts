089bbfd19ff7357973462733f67ed4ba3063541f
박준영 <sjwm98@gmail.com>
2021-09-04 17:47:12 +0900

    Add:로그관리추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Logger, Param, ParseIntPipe, Patch, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { userInfo } from 'node:os';
 import { GetUser } from 'src/auth/get-user.decorator';
@@ -12,20 +12,24 @@ import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
 @Controller('boards')
 @UseGuards(AuthGuard())
 export class BoardsController {
+    private logger = new Logger('Boards');
     constructor(private boardsService: BoardsService) {}
 
     @Get()
     getALLBoard(
         @GetUser() user: User
     ): Promise<Board[]> {
+        this.logger.verbose(`User ${user.username} trying to get all boards`)
         return this.boardsService.getAllBoards(user);
     }
 
     @Post()
     @UsePipes(ValidationPipe)
-    createBoard(@Body() CreateBoardDto: CreateBoardDto,
+    createBoard(@Body() createBoardDto: CreateBoardDto,
     @GetUser() user:User): Promise<Board> {
-        return this.boardsService.createBoard(CreateBoardDto, user);
+        this.logger.verbose(`User ${user.username} creating a new board.
+        Payload: ${JSON.stringify(createBoardDto)}`)
+        return this.boardsService.createBoard(createBoardDto, user);
 
     }
 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
-  await app.listen(3000);
+  const port =3000;
+  await app.listen(port);
+  Logger.log(`Applicating running on port ${port}`)
+  
 }
 bootstrap();
