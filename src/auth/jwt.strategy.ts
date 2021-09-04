d9f2bd836084e7495a6866464cb8223ed43e8529
박준영 <sjwm98@gmail.com>
2021-09-04 20:42:24 +0900

    Add:설정추가

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { User } from "./user.entity";
 import { UserRepository } from "./user.repository";
+import * as config from 'config';
 
 //다른곳에서도 사용할 수 있기 위해 사용
 @Injectable()
@@ -13,7 +14,7 @@ export class JwtStartegy extends PassportStrategy(Strategy) {
         private userRepository: UserRepository
     ) {
         super({
-            secretOrKey: 'Secret1234',
+            secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'),
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
         })
     }
